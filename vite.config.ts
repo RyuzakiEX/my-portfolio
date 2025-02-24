08f6242761fb923b4b5117bb68a8d1ee1fd1b9b6
RyuzakiEX <adanzajorge.dev@gmail.com>
2025-02-24 10:30:16 -0500

    fix: fix blank page and add dist files

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  assetsInclude: ['**/*.JPEG'],
+  base: '/my-portfolio/',
   plugins: [
     vue(),
     vueDevTools(),
